3440b948b7726c83cfac4b47603abdeedb8ac033
kubrawild <facunet01@icloud.com>
2024-11-22 20:50:56 -0300

    Primer App

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+
+//import TabNavigator from './src/navigation/TabNavigator';
+import MainNavigator from "./src/navigation/MainNavigator";
+
+import { store } from "./src/app/store";
+import { Provider } from "react-redux";
+
+import { createSessionsTable } from "./src/db";
+
+//import { clearSessions } from './src/db';
+
+createSessionsTable()
+  .then((result) =>
+    console.log("Tabla creada o inicializada con éxito: ", result)
+  )
+  .catch((error) => console.log("Error al crear la tabla Sessions: ", error));
+
+//clearSessions().then().catch()
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [loaded, error] = useFonts({
+    Montserrat: require("./assets/fonts/Montserrat-Variable.ttf"),
+    PressStart2P: require("./assets/fonts/PressStart2P-Static.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <MainNavigator />
+      <StatusBar style="light" />
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/firebase/database.js
@@ -0,0 +1,2 @@
+export const base_url =
+  "https://mundo-geek-59525-848d0-default-rtdb.firebaseio.com/";
